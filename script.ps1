6463d6719212b00fadc1a5b909acb891262504b1
Marcelo Buzzetti <marcelobuzzetti@gmail.com>
2024-05-08 09:39:28 -0300

    explicacao

--- script.ps1
+++ script.ps1
@@ -6,5 +6,14 @@ $output_file = "HelloDevOpsWorld_modified.md"
 
 # Substituir as strings
 (Get-Content $input_file) -replace "UNKNOWN_STUDENT", "MARCELO" -replace "UNKNOWN_TEACHER", "CAIO_ROSCELLY" | Set-Content $output_file
+# Esse é um script PowerShell que lê o conteúdo de um arquivo, realiza algumas substituições de texto e grava o resultado em um novo arquivo.
+
+# O comando Get-Content $input_file lê o conteúdo do arquivo cujo caminho é especificado pela variável $input_file.
+
+# O operador -replace é usado para substituir todas as ocorrências de um texto por outro. Neste caso, todas as ocorrências de UNKNOWN_STUDENT são substituídas por MARCELO e todas as ocorrências de UNKNOWN_TEACHER são substituídas por CAIO_ROSCELLY.
+
+# O operador | (pipe) é usado para passar o resultado das substituições para o próximo comando. Neste caso, o comando Set-Content $output_file grava o resultado das substituições no arquivo cujo caminho é especificado pela variável $output_file.
+
+# Se o arquivo de saída já existir, seu conteúdo será substituído. Se o arquivo de saída não existir, ele será criado.
 
 Write-Host "Substituição concluída. O novo arquivo foi salvo como $output_file."
--- script.sh
+++ script.sh
@@ -8,5 +8,13 @@ output_file="HelloDevOpsWorld_modified.md"
 
 # Substituir as strings
 sed -e 's/UNKNOWN_STUDENT/MARCELO/g' -e 's/UNKNOWN_TEACHER/CAIO_ROSCELLY/g' "$input_file" > "$output_file"
+# Este é um script de shell que usa o comando sed para substituir o texto em um arquivo. O comando sed é uma ferramenta de processamento de texto usada principalmente para transformar texto.
+
+# No código fornecido, o comando sed está sendo usado para substituir todas as ocorrências de UNKNOWN_STUDENT por MARCELO e todas as ocorrências de UNKNOWN_TEACHER por CAIO_ROSCELLY no arquivo de entrada especificado pela variável input_file.
+
+# O parâmetro -e permite que você adicione vários comandos de edição. Neste caso, existem dois comandos de edição: 's/UNKNOWN_STUDENT/MARCELO/g' e 's/UNKNOWN_TEACHER/CAIO_ROSCELLY/g'. O 's' indica que estamos substituindo texto, o texto entre o primeiro e o segundo '/' é o texto a ser substituído, o texto entre o segundo e o terceiro '/' é o texto pelo qual será substituído e o 'g' no final indica que todas as ocorrências devem ser substituídas (não apenas a primeira em cada linha).
+
+# O resultado da substituição é então redirecionado para um arquivo de saída especificado pela variável output_file usando o operador de redirecionamento >. Se o arquivo de saída já existir, ele será sobrescrito; se não existir, será criado.
+
 
 echo "Substituição concluída. O novo arquivo foi salvo como $output_file."
